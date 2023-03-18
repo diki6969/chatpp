7d69f01b80718bc95fa69d6b9a8dee14c7746ee7
IkyyOfficial <gendonmenjeng@gmail.com>
2023-03-18 02:41:57 +0000

    Salah siapa sok pinter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
-<script type="module">
-  import '../app.css'
+<script>
+	import '../app.css'
 </script>
 
-<div class="flex flex-col items-center py-14" id="container">
-  <slot />
+<div class="flex flex-col items-center py-14">
+	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,76 +1,63 @@
 <script lang="ts">
-  import type { CreateCompletionResponse } from 'openai'
-  import { SSE } from 'sse.js'
+	import type { CreateCompletionResponse } from 'openai'
+	import { SSE } from 'sse.js'
 
-  let context = ''
-  let loading = false
-  let error = false
-  let answer = ''
+	let context = ''
+	let loading = false
+	let error = false
+	let answer = ''
 
-  const handleSubmit = async () => {
-    loading = true
-    error = false
-    answer = ''
+	const handleSubmit = async () => {
+		loading = true
+		error = false
+		answer = ''
 
-    const eventSource = new SSE('/api/explain', {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      payload: JSON.stringify({ context })
-    })
+		const eventSource = new SSE('/api/explain', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			payload: JSON.stringify({ context })
+		})
 
-    context = ''
+		context = ''
 
-    eventSource.addEventListener('error', (e) => {
-      error = true
-      loading = false
-      console.error(e)
-      alert('Something went wrong!')
-    })
+		eventSource.addEventListener('error', (e) => {
+			error = true
+			loading = false
+			alert('Something went wrong!')
+		})
 
-    eventSource.addEventListener('message', (e) => {
-      try {
-        loading = false
+		eventSource.addEventListener('message', (e) => {
+			try {
+				loading = false
 
-        if (e.data === '[DONE]') {
-          return
-        }
+				if (e.data === '[DONE]') {
+					return
+				}
 
-        const completionResponse: CreateCompletionResponse = JSON.parse(e.data)
+				const completionResponse: CreateCompletionResponse = JSON.parse(e.data)
 
-        const [{ text }] = completionResponse.choices
+				const [{ text }] = completionResponse.choices
 
-        answer += text
-      } catch (err) {
-        error = true
-        loading = false
-        console.error(err)
-        alert('Something went wrong!')
-      }
-    })
+				answer = (answer ?? '') + text
+			} catch (err) {
+				error = true
+				loading = false
+				console.error(err)
+				alert('Something went wrong!')
+			}
+		})
 
-    eventSource.stream()
-  }
+		eventSource.stream()
+	}
 </script>
 
-<main>
-  <h2>Halo, Saya Ikyy<br>Asisten Virtualmu</h2>
-
-  <form on:submit|preventDefault={handleSubmit}>
-    <label for="context">Silahkan ketikkan pertanyaanmu<br>dikolom dibawah ini:</label>
-    <textarea name="context" rows="5" bind:value={context} />
-
-    <button disabled={!context}>Tanyakan</button>
-
-    {#if loading}
-      <p>Loading...</p>
-    {:else if error}
-      <p>Something went wrong. Please try again later.</p>
-    {:else if answer}
-      <div class="pt-4">
-        <p>Jawaban:</p>
-        <p>{answer}</p>
-      </div>
-    {/if}
-  </form>
-</main>
+<h2>Halo, Saya Ikyy<br>Asisten Virtualmu</h2> <form on:submit|preventDefault={()=>
+        handleSubmit()}> <label for="context">Silahkan ketikkan pertanyaanmu<br>dikolom dibawah ini:</label> <textarea name="context" rows="5" bind:value={context} />
+        <button>Tanyakan</button> <div class="pt-4">
+                <p>Jawaban:</p> {#if answer} <p>
+                        {answer}
+                </p>
+                {/if}
+        </div>
+</form>
